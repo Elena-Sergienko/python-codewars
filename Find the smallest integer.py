1429b345f88e1b5a3ebbac8fd8c5a7a987fad490
Elena-Sergienko <elena.serg.us@gmail.com>
2020-08-11 23:23:53 -0700

    Add Find the smallest integer in the array

---
+++ Find the smallest integer.py
@@ -0,0 +1,10 @@
+# 8 kyu
+# Find the smallest integer in the array
+# https://www.codewars.com/kata/55a2d7ebe362935a210000b2
+# Given an array of integers your solution should find the smallest integer.
+# For example:
+# Given [34, 15, 88, 2] your solution will return 2
+# Given [34, -345, -1, 100] your solution will return -345
+
+def find_smallest_int(arr):
+    return min(arr)
