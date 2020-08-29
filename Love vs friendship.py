bf9c30aac0d7616f02f73851d1a24a3a8708ab47
Elena-Sergienko <elena.serg.us@gmail.com>
2020-08-28 20:11:57 -0700

    Add Love vs friendship

---
+++ Love vs friendship.py
@@ -0,0 +1,19 @@
+# 7 kyu
+# Love vs friendship
+# https://www.codewars.com/kata/59706036f6e5d1e22d000016/solutions/javascript
+# If　a = 1, b = 2, c = 3 ... z = 26
+# Then l + o + v + e = 54
+# and f + r + i + e + n + d + s + h + i + p = 108
+# So friendship is twice stronger than love :-)
+# The input will always be in lowercase and never be empty.
+
+import string
+
+
+def words_to_marks(s):
+    alphabet = ' ' + string.ascii_lowercase
+    s1 = 0
+    for letter in s:
+        s1 += alphabet.index(letter)
+
+    return s1
